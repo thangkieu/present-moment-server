a8d8cf10b92d6f0f624f1edb00ea84ee7e4d7d58
Thang Kieu <kqthang1505@gmail.com>
2021-04-13 18:14:18 +0800

    Init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1be40a290b77a5a78e6a461396649c63'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c79cdc18-2a76-4ea5-92ef-5608a0482363'
+};
