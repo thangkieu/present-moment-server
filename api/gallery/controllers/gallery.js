992cc49d1485f710e2d7cd9e953f1e42847e1945
Thang Kieu <kqthang1505@gmail.com>
2021-04-19 12:51:48 +0800

    Add Flickr API

--- api/gallery/controllers/gallery.js
+++ api/gallery/controllers/gallery.js
@@ -1,5 +1,6 @@
 "use strict";
 const { sanitizeEntity } = require("strapi-utils");
+var Flickr = require("flickr-sdk");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
@@ -8,23 +9,37 @@ const { sanitizeEntity } = require("strapi-utils");
 
 module.exports = {
   async findRandom(ctx) {
-    const knex = strapi.connections.default;
-    const items = await knex.table("galleries").select("id");
+    const flickrKey = strapi.config.get("server.fKey");
+    const flickrAlbumId = strapi.config.get("server.fAlbumId");
+    const flickrUserId = strapi.config.get("server.fUserId");
 
-    const total = items.length;
+    const flickr = new Flickr(flickrKey);
+
+    const resp = await flickr.photosets.getPhotos({
+      photoset_id: flickrAlbumId,
+      user_id: flickrUserId,
+    });
+
+    const data = resp.body?.photoset;
+    const photos = data.photo;
+
+    const total = data.total;
     const randomIdx = Math.floor(Math.random() * total);
-    const foundItems = await strapi
-      .query("gallery")
-      .find({ id: items[randomIdx].id });
+    const foundItem = photos[randomIdx];
 
-    const item = foundItems[0];
-    if (!item) return null;
+    if (!foundItem) return null;
+
+    const photoId = foundItem.id;
+    const sourceResp = await flickr.photos.getSizes({
+      photo_id: photoId,
+    });
+
+    const sizes = sourceResp.body?.sizes.size;
 
     return sanitizeEntity(
       {
-        id: item.id,
-        isDarkBg: item.isDarkBg,
-        url: `${strapi.config.get("server.url")}${item.photo.url}`,
+        id: photoId,
+        url: sizes[sizes.length - 1].source,
       },
       { model: strapi.models.gallery }
     );
--- config/server.js
+++ config/server.js
@@ -2,6 +2,9 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   url: env("HOST_URL"),
+  fKey: env("FLICKR_KEY"),
+  fAlbumId: env("FLICKR_ALBUM_ID"),
+  fUserId: env("FLICKR_USER_ID"),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "1be40a290b77a5a78e6a461396649c63"),
