579aa0a22d2cd68a1912c0e9872fe71d3512a7b6
Thang Kieu <kqthang1505@gmail.com>
2021-04-19 19:24:01 +0800

    Update colors

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   url: env("HEROKU_URL"),
-  fKey: env("FLICKR_KEY"),
-  fAlbumId: env("FLICKR_ALBUM_ID"),
-  fUserId: env("FLICKR_USER_ID"),
+  fKey: env("FLICKR_KEY", "26e2deb0324c202dde15596c896a6268"),
+  fAlbumId: env("FLICKR_ALBUM_ID", "72157718959970459"),
+  fUserId: env("FLICKR_USER_ID", "100891623@N08"),
 });
